3de540156b965f30c0ce987c40717078972566d7
Julia Kutsyn <yuliyaflm@gmail.com>
2021-05-10 19:14:31 +0300

    adds auxiliary js files and css

---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
